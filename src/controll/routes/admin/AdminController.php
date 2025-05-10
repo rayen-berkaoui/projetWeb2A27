97d3537f8e1a4190f2f94c33793dad52d7e1de52
zidilyes <ilyeszid33@gmail.com>
2025-05-10 14:10:48 +0100

    comment456789

--- src/controll/routes/admin/AdminController.php
+++ src/controll/routes/admin/AdminController.php
@@ -1,22 +1,12 @@
 <?php
 class AdminController {
     protected function render($view, $data = []) {
-<<<<<<< HEAD
-        function view($path) {
-            return __DIR__ . '/../../../views/' . $path . '.php';
-            require $this->view('admin/layout');
-            extract($data);
-            $content = view($view, $data);
-            require view('admin/layout');
-        }
-=======
         $viewPath = __DIR__ . '/../../../views/' . $view . '.php';
         extract($data);
         ob_start();
         require $viewPath;
         $content = ob_get_clean();
         require __DIR__ . '/../../../views/admin/layout.php';
->>>>>>> GestionForums
     }
     
     protected function model($model) {
