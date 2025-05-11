5299d091113c3c2a0f55c492e75d4d8ec7e6aef8
zidilyes <167861137+zidilyes@users.noreply.github.com>
2025-05-11 03:24:25 +0200

    Add files via upload

---
+++ controller/PostFrontController.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . '/../../../domain/post.php';
+
+class PostFrontController {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function index() {
+        $posts = Post::getAll($this->db);
+        require_once __DIR__ . '/../../../../view/user/pages/posts/list.php';
+    }
+
+    public function create() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+
+            $post = new Post(null, $title, $content);
+            $post->save($this->db);
+
+            header('Location: /2A27/posts');
+            exit;
+        } else {
+            require_once __DIR__ . '/../../../../view/user/pages/posts/create.php';
+        }
+    }
+}
